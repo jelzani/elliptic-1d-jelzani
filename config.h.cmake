fec95f615ef876a7ebf7e3feb8382725fec0cfe9
Jelena Zaninovic <jelzani@pr1-03.studenti.math.hr>
2023-04-12 19:16:37 +0200

    Initial commit.

---
+++ config.h.cmake
@@ -0,0 +1,45 @@
+/* begin elliptic-1d
+   put the definitions for config.h specific to
+   your project here. Everything above will be
+   overwritten
+*/
+
+/* begin private */
+/* Name of package */
+#define PACKAGE "@DUNE_MOD_NAME@"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "@DUNE_MAINTAINER@"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@DUNE_MOD_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@DUNE_MOD_NAME@ @DUNE_MOD_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@DUNE_MOD_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "@DUNE_MOD_URL@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@DUNE_MOD_VERSION@"
+
+/* end private */
+
+/* Define to the version of elliptic-1d */
+#define ELLIPTIC_1D_VERSION "@ELLIPTIC_1D_VERSION@"
+
+/* Define to the major version of elliptic-1d */
+#define ELLIPTIC_1D_VERSION_MAJOR @ELLIPTIC_1D_VERSION_MAJOR@
+
+/* Define to the minor version of elliptic-1d */
+#define ELLIPTIC_1D_VERSION_MINOR @ELLIPTIC_1D_VERSION_MINOR@
+
+/* Define to the revision of elliptic-1d */
+#define ELLIPTIC_1D_VERSION_REVISION @ELLIPTIC_1D_VERSION_REVISION@
+
+/* end elliptic-1d
+   Everything below here will be overwritten
+*/
---
+++ dune/elliptic-1d/elliptic-1d.hh
@@ -0,0 +1,6 @@
+#ifndef ELLIPTIC_1D_HH
+#define ELLIPTIC_1D_HH
+
+// add your classes here
+
+#endif // ELLIPTIC_1D_HH
